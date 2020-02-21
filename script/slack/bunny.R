2caa4f8c35bd51722a25ccda27372dfeb69b1664
Jonathan Sidi <yonis@metrumrg.com>
2020-02-21 09:02:57 -0500

    update slackreprex

--- script/slack/bunny.R
+++ script/slack/bunny.R
@@ -2,35 +2,50 @@ library(magick)
 library(bunny)
 
 bg_color <- "#ffffff"
-bg_color2 <- "#f6f3f9"
-fg_color <- "#042a2b"
-fgd_color1 <- "#772e25"
-fgd_color2 <- "#411815"
+fg_color <- "#218a7dff"
 
 sandbox <- image_read("script/slack/sandbox.png")
 
-blue <- image_read("script/slack/slack-1.svg") %>%
-  image_scale("10%")
-
-red <- image_read("script/slack/slack-2.svg") %>%
-  image_scale("10%")
-
-yellow <- image_read("script/slack/slack-3.svg")
-
-green <- image_read("script/slack/slack-4.svg")%>%
-  image_scale("60%")%>%
-  image_composite(sandbox%>%image_scale("200%"),
+green <- image_read("script/slack/slack-4.png")%>%
+  image_scale("65%")%>%
+  image_composite(sandbox%>%image_scale("500%"),
                   gravity = "center")
 
-image_canvas_hex(fill_color = bg_color, 
+green_hex <- image_canvas_hex(
+  fill_color = bg_color, 
   border_color = fg_color, 
   border_size = 5)%>%
-  image_composite(green,
-  gravity = "center") %>%
+  image_composite(green,gravity = "north",offset = "+0-250") %>%
   image_scale("20%")%>%
   image_annotate("slackreprex", 
+                 color = fg_color,
                  gravity = "center", 
                  location = "+0+100", 
-                 size=30,
-                 weight = 400)%>%
-  image_write('script/slack/green_hex.png')
+                 size=45,
+                 weight = 400)
+
+green_hex %>%
+  image_scale("1200x1200") %>%
+  image_write("images/hex/slackreprex.png", density = 600)
+
+green_hex %>%
+  image_scale("200x200") %>%
+  image_write("images/logos/slackreprex.png", density = 600)
+
+green_hex_gh <- green_hex %>%
+  image_scale("400x400")
+
+gh_logo <- bunny::github %>%
+  image_scale("40x40")
+
+shred_ghcard <- image_canvas_ghcard(fill_color = bg_color2) %>%
+  image_composite(green_hex_gh, gravity = "East", offset = "+100+0") %>%
+  image_annotate("Reprex for Slack ", gravity = "West", location = "+60-30", 
+                 style = "italic", color=fg_color, size=65, font="Volkhov", weight = 700) %>%   
+  image_compose(gh_logo, gravity="West", offset = "+60+40") %>%
+  image_annotate("yonicd/slackreprex", gravity="West", 
+                 location="+110+45", size=38, font="Ubuntu Mono") %>%
+  image_border_ghcard(bg_color2)
+
+shred_ghcard %>%
+  image_write("images/cards/slackreprex_ghcard.png", density = 600)
